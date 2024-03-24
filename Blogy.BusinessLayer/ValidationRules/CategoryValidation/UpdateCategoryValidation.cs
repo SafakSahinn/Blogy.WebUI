6469bde002b436ddc015534c4912eec0fd022ec4
Şafak Şahin <safaksahin022@gmail.com>
2024-03-24 13:16:13 +0300

    updatecategoryvalidation

---
+++ Blogy.BusinessLayer/ValidationRules/CategoryValidation/UpdateCategoryValidation.cs
@@ -0,0 +1,20 @@
+﻿using Blogy.EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.BusinessLayer.ValidationRules.CategoryValidation
+{
+    public class UpdateCategoryValidation : AbstractValidator<Category>
+    {
+        public UpdateCategoryValidation()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz!");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı en az 3 karekterden oluşmalıdır!");
+            RuleFor(x => x.CategoryName).MaximumLength(30).WithMessage("Kategori adı en fazla 30 karekterden oluşmalıdır!");
+        }
+    }
+}
