b825d196e1425b9198e2ee643a8be7f08ace3588
Şafak Şahin <safaksahin022@gmail.com>
2024-03-24 14:24:48 +0300

    categorycontroller

---
+++ Blogy.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoryController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public IActionResult CategoryList()
+		{
+			var values = _categoryService.TGetListAll();
+			return View(values);
+		}
+	}
+}
