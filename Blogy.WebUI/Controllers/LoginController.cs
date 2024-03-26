1c898559742f4ebc9021a76e1579cefcd68f53d9
Şafak Şahin <safaksahin022@gmail.com>
2024-03-26 14:27:00 +0300

    asyc

---
+++ Blogy.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Blogy.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<AppUser> _singInManager;
+
+		public LoginController(SignInManager<AppUser> singInManager)
+		{
+			_singInManager = singInManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(UserSingInView)
+	}
+}
--- Blogy.WebUI/Controllers/RegisterController.cs
+++ Blogy.WebUI/Controllers/RegisterController.cs
@@ -9,43 +9,51 @@ namespace Blogy.WebUI.Controllers
 	{
 		private readonly UserManager<AppUser> _userManager;
 
-        public RegisterController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
 		[HttpGet]
-        public IActionResult Index()
+		public IActionResult Index()
 		{
 			return View();
 		}
 
-        [HttpPost]
-        public async Task<IActionResult> Index(CreateRegisterViewModel p)
-        {
-            AppUser appUser = new AppUser()
-            {
-                Name = p.Name,
-                Email = p.Email,
-                Surname = p.Surname,
-                UserName = p.UserName,
-                Description = "aa",
-                ImageUrl = "bb"
-            };
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateRegisterViewModel p)
+		{
+			if (p.Password != null)
+			{
+				AppUser appUser = new AppUser()
+				{
+					Name = p.Name,
+					Email = p.Email,
+					Surname = p.Surname,
+					UserName = p.UserName,
+					Description = "aa",
+					ImageUrl = "bb"
+				};
 
-            var result = await _userManager.CreateAsync(appUser, p.Password);
-            if(result.Succeeded)
-            {
-                return RedirectToAction("Index", "Login");
-            }
-            else
-            {
-                foreach(var item in result.Errors)
-                {
-                    ModelState.AddModelError("", item.Description);
-                }
-            }
-            return View();
-        }
-    }
+				var result = await _userManager.CreateAsync(appUser, p.Password);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
+				return View();
+			}
+			else
+			{
+				ModelState.AddModelError("", "Şifre alanı boş geçilemez");
+				return View();
+			}
+		}
+	}
 }
